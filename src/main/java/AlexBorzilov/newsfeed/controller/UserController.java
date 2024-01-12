aa0d498406e4a681f150406ffc5920e30b422229
AlexBorzilov <a.borzilov@mk.dunice.net>
2024-01-12 17:40:18 +0300

    features/1 - add UserContoller and method all

---
+++ src/main/java/AlexBorzilov/newsfeed/controller/UserController.java
@@ -0,0 +1,19 @@
+package AlexBorzilov.newsfeed.controller;
+
+
+import AlexBorzilov.newsfeed.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/user")
+@RequiredArgsConstructor
+public class UserController {
+    private final UserService userService;
+
+    @GetMapping
+    public ResponseEntity<?> all() {
+        return ResponseEntity.ok(userService.all());
+    }
+}
